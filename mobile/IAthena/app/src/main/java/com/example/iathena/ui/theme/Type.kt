3384a0c4ca5376688a3d24089082acc7beb69916
RafaelBrasil1 <rafaelbr.ulbrich@gmail.com>
2026-05-21 11:58:51 -0300

    tela carregamento kotlin

--- mobile/IAthena/app/src/main/java/com/example/iathena/ui/theme/Type.kt
+++ mobile/IAthena/app/src/main/java/com/example/iathena/ui/theme/Type.kt
@@ -2,11 +2,18 @@ package com.example.iathena.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.iathena.R
 
 // Set of Material typography styles to start with
+val NunitoFont = FontFamily(
+    Font(resId = R.font.nunito_extrabold, weight = FontWeight.Normal),
+    // Se tiver a versão em negrito separada, você pode adicionar abaixo:
+    // Font(resId = R.font.minha_fonte_customizada_bold, weight = FontWeight.Bold)
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -15,6 +22,7 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
