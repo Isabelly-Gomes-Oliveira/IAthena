08be2bbea550cd407eeb5bc8e97260139b2c0187
Isabelly Gomes de Oliveira <gomesdeoliveira445@gmail.com>
2026-05-28 11:13:29 -0300

    Correção

--- backend/API/services/GenerateService.js
+++ backend/API/services/GenerateService.js
@@ -7,19 +7,20 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 class GenerateService {
 
     async gerarTexto(textoPesquisa) {
-        try {
-            const response = await ai.models.generateContent({
-                model: 'gemini-2.5-flash',
-                contents: `Faça um resumo COERENTE e CURTO com o conteúdo: ${textoPesquisa}`,
-            });
+    try {
+        const response = await ai.models.generateContent({
+            model: 'gemini-2.5-flash',
+            contents: `Faça um resumo COERENTE e CURTO com o conteúdo: ${textoPesquisa}`,
+        });
 
-            return response.text;
+        const texto = response.candidates[0].content.parts[0].text;
+        return texto;
 
-        } catch (error) {
-            console.error("Erro ao gerar resumo:", error);
-            return new Error("Erro ao gerar resumo");
-        }
+    } catch (error) {
+        console.error("Erro ao gerar resumo:", error);
+        return new Error("Erro ao gerar resumo");
     }
+}
 
     async resumirPesquisa(textoPesquisa) {
         const resumo = await this.gerarTexto(textoPesquisa);
