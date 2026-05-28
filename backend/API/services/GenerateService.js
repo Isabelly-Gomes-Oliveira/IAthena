fe029d4dcb70b1db6ecd0bbcc70334d5e780df19
Isabelly Gomes de Oliveira <gomesdeoliveira445@gmail.com>
2026-05-28 10:12:59 -0300

    Aplicação inicial da IA generativa do Gemini

--- backend/API/services/GenerateService.js
+++ backend/API/services/GenerateService.js
@@ -1,11 +1,38 @@
+import { GoogleGenAI } from '@google/genai';
+
+// Inicializa a SDK usando a sua variável de ambiente
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
 class GenerateService {
 
-    resumirPesquisa (textoPesquisa){
-        // Apenas um teste simples
-        // Finge que aqui há um processamento da pesquisa na ia e depois o resultado é guardado em "resumo"
-        const resumo = "O resumo da pesquisa: " + textoPesquisa + " foi feito"
-        return resumo;
+    // Chamada da IA Generativa do Gemini
+    gerarTexto(textoPesquisa) {
+        try {
+            const response = ai.models.generateContent({
+            model: 'gemini-2.5-flash', // modelo do Gemini
+            contents: `Faça um resumo COERENTE e CURTO com o conteúdo: ${textoPesquisa}`,
+            });
+            
+            return response.text;
+
+        } catch (error) {
+            return error("Erro ao gerar resumo:", error);
+        }
     }
+
+
+    resumirPesquisa (textoPesquisa){
+        
+        const resumo = this.gerarTexto(textoPesquisa);
+
+        if (this.gerarTexto instanceof Error){
+            return "O resumo não pôde ser gerado :( "
+        }
+        else {
+            return resumo;
+        }
+    }
+
 }
 
 module.exports = {GenerateService};
