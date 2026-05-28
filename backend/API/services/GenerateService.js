da0e1c9364a078fe4eb25d9246e923fec6c97ad8
Isabelly Gomes de Oliveira <gomesdeoliveira445@gmail.com>
2026-05-28 10:46:06 -0300

    Correções

--- backend/API/services/GenerateService.js
+++ backend/API/services/GenerateService.js
@@ -1,39 +1,35 @@
 const { GoogleGenAI } = require('@google/genai');
-const dotenv = require(dotenv);
+const dotenv = require('dotenv');
+dotenv.config();
 
-// Inicializa a SDK usando a sua variável de ambiente
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 class GenerateService {
 
-    // Chamada da IA Generativa do Gemini
-    gerarTexto(textoPesquisa) {
+    async gerarTexto(textoPesquisa) {
         try {
-            const response = ai.models.generateContent({
-            model: 'gemini-2.5-flash', // modelo do Gemini
-            contents: `Faça um resumo COERENTE e CURTO com o conteúdo: ${textoPesquisa}`,
+            const response = await ai.models.generateContent({
+                model: 'gemini-2.5-flash',
+                contents: `Faça um resumo COERENTE e CURTO com o conteúdo: ${textoPesquisa}`,
             });
-            
+
             return response.text;
 
         } catch (error) {
-            return error("Erro ao gerar resumo:", error);
+            console.error("Erro ao gerar resumo:", error);
+            return new Error("Erro ao gerar resumo");
         }
     }
 
+    async resumirPesquisa(textoPesquisa) {
+        const resumo = await this.gerarTexto(textoPesquisa);
 
-    resumirPesquisa (textoPesquisa){
-        
-        const resumo = this.gerarTexto(textoPesquisa);
+        if (resumo instanceof Error) {
+            return "O resumo não pôde ser gerado :( ";
+        }
 
-        if (this.gerarTexto instanceof Error){
-            return "O resumo não pôde ser gerado :( "
-        }
-        else {
-            return resumo;
-        }
+        return resumo;
     }
-
 }
 
-module.exports = {GenerateService};
+module.exports = { GenerateService };
